f292f331dcf856bc93a17828a082ebea9796e3ec
Levi <shrieverdev@gmail.com>
2021-04-28 16:04:32 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className='App'>
+      <form>
+        <input type="text"/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
