1ed8352beab399837c7da52e0fa183e91fb88f27
Levi <shrieverdev@gmail.com>
2021-04-28 16:35:37 -0700

    set up bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form } from "react-bootstrap";
 
 function App() {
   return (
     <div className='App'>
-      <form>
-        <input type="text"/>
-      </form>
+      <h1 className="text-center">Stylish Compound Interest Calculator</h1>
+      <Form>
+        <input type='text' />
+      </Form>
     </div>
   );
 }
