3dc3468407f621570ea007bd9879a5f6b22dd6d3
Levi <shrieverdev@gmail.com>
2021-10-02 20:57:39 -0500

    fixed inputs

--- src/utils/findInputSource.ts
+++ src/utils/findInputSource.ts
@@ -1,6 +1,6 @@
-import { U } from "./interfaces";
+import { UserData } from "./interfaces";
 
-const findInputSource = (e: any, prevState: U) => {
+const findInputSource = (e: any, prevState: UserData) => {
   const inputName: string = e.target.name;
   const inputValue: number = parseInt(e.target.value);
   if (inputName === "principal") {
