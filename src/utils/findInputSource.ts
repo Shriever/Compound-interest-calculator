826e43b09da5342c40338a9fffe0c6c1e1666923
Levi <shrieverdev@gmail.com>
2021-10-09 19:26:52 -0500

    format

--- src/utils/findInputSource.ts
+++ src/utils/findInputSource.ts
@@ -3,6 +3,7 @@ import { UserData } from "./interfaces";
 const findInputSource = (e: any, prevState: UserData) => {
   const inputName: string = e.target.name;
   const inputValue: number = parseInt(e.target.value);
+
   if (inputName === "principal") {
     return {
       ...prevState,
