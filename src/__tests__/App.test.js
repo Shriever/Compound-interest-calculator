a57ca043c160bcc8778dde1bf599cf43b37ecfba
Levi Lieberman <ShrieverDev@gmail.com>
2021-11-01 16:44:57 -0700

    first test working

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
+import { BrowserRouter } from 'react-router-dom';
 
 test('works', () => {
-  const screen = render(<App />);
+  const screen = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 
   expect(screen.getByTestId('header')).toBeInTheDocument();
 });
